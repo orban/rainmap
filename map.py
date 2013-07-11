6c72e94994c20ee9d46a7fa05da0514a8a1c7a0a
Ryan Orban <ryan@ryanorban.com>
2013-07-10 19:47:30 -0700

    Added shapefile support

--- map.py
+++ map.py
@@ -2,25 +2,28 @@
 
 from math import cos, sin, atan2, pi, sqrt
 from collections import namedtuple
-import urllib2
+import shapefile
 import json
+import point_store
+import urllib2
 
 
-Point = namedtuple('Point', 'lat lon')
+
+Point = namedtuple('Point', 'lon lat')
 	
 
 def center_geolocation(geolocations):
 	"""
 	Provide a relatively accurate center lat, lon returned as a list pair,
 	givena list of list pairs.
-	ex: in: geolocations = ((lat1,lon1), (lat2,lon2),)
+	ex: in: geolocations = ((lon1,lat1), (lon2,lat2),)
 		out: (center_lat, center_lon)
 	"""
 	x = 0
 	y = 0
 	z = 0
 
-	for lat, lon in geolocations:
+	for lon, lat in geolocations:
 		lat = float(lat) * pi/180
 		lon = float(lon) * pi/180
 		x += cos(lat) * cos(lon)
@@ -38,12 +41,17 @@ def main():
 		'http://s3.amazonaws.com/static.m2i.stamen/latest_images.json')
 	data = json.load(response)
 	
+
+	points = []
+	w = shapefile.Writer(shapefile.POINT)
 	for record in data['items']:
 		coords = record['coords'].split(":")
 		if len(coords) > 2:
 			pt1 = Point(coords[0], coords[1])
 			pt2 = Point(coords[2], coords[3])
-			print pt1, pt2, center_geolocation([pt1, pt2])
+			center = center_geolocation([pt1, pt2])
+			w.point(center.lon, center.lat, 0)
+	w.save('shapefile.shp')
 		
 if __name__ == '__main__':
 	main()
